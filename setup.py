9e165645e3d8d647185b31c981e85644f011e2ac
Grahame Bowland <grahame@angrygoats.net>
2016-06-23 15:25:37 +0800

    fix the maturity field

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.0.19',
+    version='0.0.20',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
