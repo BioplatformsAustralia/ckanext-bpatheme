bd5f8a86e8c76be41e0fe94912b89a5d6ec73317
Grahame Bowland <grahame@angrygoats.net>
2016-06-23 16:53:35 +0800

    wheat_cultivars: add in the notes field

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.0.20',
+    version='0.0.21',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
