ccc146b9d631b84044c2e24e1e43ef4d46729c68
Grahame Bowland <grahame@angrygoats.net>
2016-07-20 16:38:56 +0800

    fix up JSON errors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.0.22',
+    version='0.0.23',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
