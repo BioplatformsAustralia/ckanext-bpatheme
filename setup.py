9eb6eb70288867d12d499f45c1d1f6dbef948e28
Grahame Bowland <grahame@angrygoats.net>
2016-07-20 16:51:39 +0800

    minor pathogen schema update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.0.23',
+    version='0.0.24',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
