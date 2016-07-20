563ec8be4ac62f61833dade1073dfae0f5e0ec9b
Grahame Bowland <grahame@angrygoats.net>
2016-07-20 16:20:21 +0800

    draft schema for wheat-pathogens

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.0.21',
+    version='0.0.22',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
