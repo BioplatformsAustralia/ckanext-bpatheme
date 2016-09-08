c1cc1755ec49ec5a8d2b04db07d9d8f5a0f153c9
Grahame Bowland <grahame@angrygoats.net>
2016-09-08 20:26:01 +0800

    bump version; static assets for projects

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.1.1',
+    version='0.1.2',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
