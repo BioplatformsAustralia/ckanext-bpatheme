a8ab4650d52fc99238c1e90a5217b778fa5c5e4a
Grahame Bowland <grahame@angrygoats.net>
2016-09-13 22:23:29 +0800

    fix HTTPS error, favicon

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.1.11',
+    version='0.1.12',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
