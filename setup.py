56751f52d1e5c73cba148801e17e99897cdc016c
Grahame Bowland <grahame@angrygoats.net>
2016-10-17 14:25:48 +0800

    add dataset fields for sepsis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.1.12',
+    version='0.1.13',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
