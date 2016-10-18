67f00fadf3681807f021ca318a0794a0e43e4236
Grahame Bowland <grahame@angrygoats.net>
2016-10-18 16:00:05 +0800

    tweak access message

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.5.1',
+    version='0.5.2',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
