8fd01e603c1388b19fe916065e0eabca41f739de
Grahame Bowland <grahame@angrygoats.net>
2017-01-25 15:06:11 +0800

    MM - add flowid

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.14.3',
+    version='0.14.4',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
