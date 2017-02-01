2a7535c40f1d463fc35eaf5ac31773eafa03088e
Grahame Bowland <grahame@angrygoats.net>
2017-02-01 16:19:09 +0800

    MM amplicon update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.15.1',
+    version='0.15.2',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
