3a3bcc73478ee07cbd8ee582bfaa51128c5efd49
Grahame Bowland <grahame@angrygoats.net>
2017-04-03 16:25:27 +0800

    MM: add tracking fields

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.28.2',
+    version='0.29.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
