87370ede6d76d0318afb0d3ffc64a2bb5570a059
Grahame Bowland <grahame@angrygoats.net>
2017-04-03 11:05:31 +0800

    add a bpa_id to singlecellrnaseq

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.28.0',
+    version='0.28.2',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
