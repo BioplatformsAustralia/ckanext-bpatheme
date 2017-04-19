202df37f4e437c673abdf142dd1ccac60189bbb4
Grahame Bowland <grahame@angrygoats.net>
2017-04-19 15:57:57 +0800

    BASE genomics amplicon

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.31.3',
+    version='0.32.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
