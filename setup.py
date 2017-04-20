016ff32d763a2c81d8fc713cd12eb3db58435c33
Grahame Bowland <grahame@angrygoats.net>
2017-04-20 12:47:14 +0800

    BASE amplicons contextual data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.32.0',
+    version='0.32.1',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
