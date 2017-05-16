e3b16d97c8ce93cafa0ae781381db3ed2d16ff91
Grahame Bowland <grahame@angrygoats.net>
2017-05-16 11:31:43 +0800

    change message when no datasets available and not logged in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.41.0',
+    version='0.42.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
