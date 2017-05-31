e76a9f81547c4d797dd36d3ed96fb8408589615f
Grahame Bowland <grahame@angrygoats.net>
2017-05-31 10:51:14 +0800

    add BASE Genomics Amplicon Control data type

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.48.0',
+    version='0.49.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
