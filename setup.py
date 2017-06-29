0b79bca200977ceba5ad31ef832a05cb69564f5d
Grahame Bowland <grahame@angrygoats.net>
2017-06-29 14:42:03 +0800

    MM NCBI: initial extra fields

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.58.0',
+    version='0.59.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
