8533a45b68c0c1d83daed5195bbd50eb367c16aa
Grahame Bowland <grahame@angrygoats.net>
2017-06-29 14:17:41 +0800

    BASE NCBI submission: further new fields

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.56.0',
+    version='0.58.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
