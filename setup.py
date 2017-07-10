6aebe7b96a6c57d44fafe0b05c6e638d5b73eea5
Grahame Bowland <grahame@angrygoats.net>
2017-07-10 13:17:50 +0800

    SC: new contextual metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.60.2',
+    version='0.61.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
