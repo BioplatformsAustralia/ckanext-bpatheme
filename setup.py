c539722df544f096804591c325ac61e267f34c01
Grahame Bowland <grahame@angrygoats.net>
2017-08-18 15:54:13 +0800

    update BASE search link; release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.66.0',
+    version='0.67.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
