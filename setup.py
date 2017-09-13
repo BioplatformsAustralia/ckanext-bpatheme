51f9f8b02af65bf5c3d51f825b3c9531b1bc7cd5
Grahame Bowland <grahame@angrygoats.net>
2017-09-13 11:59:40 +0800

    OMG 10X raw (new workflow)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.70.1',
+    version='0.71.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
