53256c2c5e3f264e2ea2af964a9cb6c86ccedd33
Grahame Bowland <grahame@angrygoats.net>
2017-10-18 15:51:42 +0800

    ARP Proteomics 2D Library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.73.0',
+    version='0.74.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
