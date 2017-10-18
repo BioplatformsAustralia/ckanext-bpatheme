ce533bca85bfc9a64f4b697bf22c8d16a854dfa4
Grahame Bowland <grahame@angrygoats.net>
2017-10-18 16:26:59 +0800

    resource fields for 2D Library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.75.0',
+    version='0.75.1',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
