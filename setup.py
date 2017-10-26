69d2fcccd766418a394d51f2b254f94748eab544
Grahame Bowland <grahame@angrygoats.net>
2017-10-26 15:07:44 +0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.75.1',
+    version='0.76.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
