b2bb119a2b7c4c446df7aa6d301f65d10482b99c
Grahame Bowland <grahame@angrygoats.net>
2017-11-22 15:30:41 +0800

    further OMG schema updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.76.5',
+    version='0.76.6',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
