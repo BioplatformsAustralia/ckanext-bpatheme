85d8e8178892a6d8097b59c43aa07f6da327f98f
Grahame Bowland <grahame@angrygoats.net>
2017-11-23 10:24:59 +0800

    OMG genomics hiseq theme update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.76.6',
+    version='0.76.7',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
