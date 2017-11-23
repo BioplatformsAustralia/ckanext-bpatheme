0ee371f87e43dc0d9d1c573ceecc59d2b14fd60f
Grahame Bowland <grahame@angrygoats.net>
2017-11-23 11:23:02 +0800

    OMG 10X Processed update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.76.10',
+    version='0.76.11',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
