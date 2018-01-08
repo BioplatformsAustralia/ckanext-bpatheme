2524478632b216128f02149e4147688efe24a308
Grahame Bowland <grahame@angrygoats.net>
2018-01-08 18:21:46 +0800

    update schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.79.15',
+    version='0.79.16',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
