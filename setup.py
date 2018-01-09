4a0a6c7fe035249d97b04c1e8bb695a35dbfc3b1
Sam Chang <schang@ccg.murdoch.edu.au>
2018-01-09 11:57:09 +0800

    bump version to 0.79.18

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.79.17',
+    version='0.79.18',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
