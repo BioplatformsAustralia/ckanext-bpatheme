c4feeb26d5671997549e5dc1af66de3cd03fb941
Grahame Bowland <grahame@angrygoats.net>
2018-03-23 20:16:55 +0800

    regenerate schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.80.10',
+    version='0.80.11',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
