46235237d2c6704e0748e6d5abbad7c10c77f539
Sam Chang <schang@ccg.murdoch.edu.au>
2018-04-12 12:48:17 +0800

    increase version to 0.80.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.80.12',
+    version='0.80.13',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
