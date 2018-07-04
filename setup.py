584901a35ee4af20b7a7d3a63ea06cff4a8fe2d0
Sam <schang@ccg.murdoch.edu.au>
2018-07-04 13:27:10 +0800

    update to 0.80.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.80.19',
+    version='0.80.20',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
