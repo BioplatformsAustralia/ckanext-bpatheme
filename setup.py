bef938bf8c4f04f37b85d80986a36340e72c3c22
Sam <schang@ccg.murdoch.edu.au>
2018-07-04 12:22:54 +0800

    bump version to 0.80.18

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.80.17',
+    version='0.80.18',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
