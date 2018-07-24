aee2581c48187756fffa024315a4e10a563d1b75
Sam <schang@ccg.murdoch.edu.au>
2018-07-24 11:44:59 +0800

    retag to 0.81.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.80.23',
+    version='0.81.1',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
