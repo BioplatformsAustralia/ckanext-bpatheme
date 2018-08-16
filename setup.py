d5cfeebea8f40dd93e158e7c2e6ac2cfd12e2371
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-08-16 10:51:21 +0800

    fix up plants logo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.82.1',
+    version='0.82.2',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
