8d1c091c3453ff9f175e0023cbd353a6ccf4791a
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-08-29 11:52:11 +0800

    update the plants logo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.0',
+    version='0.84.1',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
