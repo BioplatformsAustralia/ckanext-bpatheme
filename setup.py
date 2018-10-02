17a292b2c101c85f9755c3dba01b83a960fdbec5
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-10-02 15:44:07 +0800

    AM: buttons larger, centred

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.10',
+    version='0.84.11',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
