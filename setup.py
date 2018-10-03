ae39da465c9df3173a4faa036ae5f13dee0e22ad
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-10-03 14:30:43 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.11',
+    version='0.84.12',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
