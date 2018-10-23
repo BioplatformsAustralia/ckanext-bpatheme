b426cceddca585595aabd24820fe25d2e983ca9e
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-10-23 11:08:30 +0800

    remove barcode from registration dropdown

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.13',
+    version='0.84.14',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
