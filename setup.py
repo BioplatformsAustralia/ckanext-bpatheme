11ac95b8915c38a696bb1273c4f07ed0b8152789
krupalip <kpatel@ccg.murdoch.edu.au>
2018-10-25 11:22:17 +0800

    next release for #734

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.16',
+    version='0.84.17',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
