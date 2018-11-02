b39a66171df70ad701b879374527af25b0385ab8
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-11-02 11:43:09 +1100

    remove Melanoma from projects of interest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.18',
+    version='0.84.19',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
