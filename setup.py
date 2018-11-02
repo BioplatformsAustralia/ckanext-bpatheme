ec92f597bebef689b825a4fb93d480c1f8b1175e
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-11-02 11:59:27 +1100

    add plants to registration drop-down

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.84.19',
+    version='0.84.20',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
