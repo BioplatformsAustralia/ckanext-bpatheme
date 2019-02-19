8ef4f9072895e50b7233078994e2213710148e55
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2019-02-19 11:47:20 +0800

    update OMG theme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.86.2',
+    version='0.87.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
