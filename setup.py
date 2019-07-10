2acb4a674fe8f45f3958f40fef88fb5ebf1748bb
Grahame Bowland <grahame@oreamnos.com.au>
2019-07-10 15:47:50 +0800

    add missing file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.91.3',
+    version='0.91.4',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
