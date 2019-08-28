51ffe99db0e3b13666a1ef95a6c10b7460e1bf27
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-08-28 23:13:35 +0800

    OMG,GAP: schema update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.91.6',
+    version='0.91.7',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
