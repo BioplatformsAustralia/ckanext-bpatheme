ea94bc9be870238080ea2ec716d51a7da22fd323
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-10-24 13:03:42 +0800

    release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.92.1',
+    version='0.93.0',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
