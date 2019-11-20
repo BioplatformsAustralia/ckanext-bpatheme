684d6134aa390c5de6f5a39146cc4fb76c0e22aa
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-11-20 13:28:08 +0800

    rename theme throughout (upstream inactive); update schemas

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# data.wa.gov.au
+# CKAN theme for BPA
 
-This CKAN extension will contain customisations and styling for data.wa.gov.au, including:
+This CKAN extension will contain customisations and styling for data.bioplatforms.com including:
 
 * stylesheet (to be similar to data.wa.gov.au, based on readysteadygov template)
 * custom metadata schema (to be used with ckanext-scheming)
 * custom templates (e.g. geospatial coverate preview, page header)
+
+Credit: based upon the data.wa.gov.au theme
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name='ckanext-datawagovautheme',
+    name='ckanext-bpathemetheme',
     version='2.0.2',
     description='',
     license='AGPL3',
@@ -10,15 +10,15 @@ setup(
     author_email='florian.mayer@dpaw.wa.gov.au',
     url='http://govhack2015.readthedocs.org/',
     namespace_packages=['ckanext'],
-    packages=['ckanext.datawagovautheme'],
+    packages=['ckanext.bpathemetheme'],
     zip_safe=False,
     include_package_data=True,
-    package_dir={'ckanext.datawagovautheme': 'ckanext/datawagovautheme'},
-    package_data={'ckanext.datawagovautheme': ['*.json', 'fanstatic/*.js', 'fanstatic/styles/*.css', 'fanstatic/styles/*.scss', 'templates/*.html', 'templates/*/*.html',
+    package_dir={'ckanext.bpathemetheme': 'ckanext/bpathemetheme'},
+    package_data={'ckanext.bpathemetheme': ['*.json', 'fanstatic/*.js', 'fanstatic/styles/*.css', 'fanstatic/styles/*.scss', 'templates/*.html', 'templates/*/*.html',
                                                'templates/*/*/*.html', 'static/datacats/*.png', 'static/fonts/arial/*.eot', 'static/fonts/arial/*.svg', 'static/fonts/arial/*.woff', 'static/fonts/arial/*.woff2', 'static/fonts/arial/*.ttf', 'static/*.png', 'static/*.ico']},
 
     entry_points="""
         [ckan.plugins]
-        datawagovau_theme = ckanext.datawagovautheme.plugins:CustomTheme
+        bpatheme_theme = ckanext.bpathemetheme.plugins:CustomTheme
     """
 )
