2e6251d35cd23be1cbffabdae0ce6c4d1af4f8d0
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-12-03 10:57:05 +0800

    merge stuffup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.94.2',
+    version='0.94.3',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
