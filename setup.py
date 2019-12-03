1cf43f7f6f364bf7163a73ccae0b9ee93a6aa05c
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-12-03 12:43:13 +0800

    new favicon

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='0.94.3',
+    version='0.94.4',
     description='',
     license='AGPL3',
     author='CCG, Murdoch University',
