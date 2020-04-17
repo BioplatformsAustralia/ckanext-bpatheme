9c258b1397fc2c13f8d28490620c3f655ae2ad80
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-04-17 09:44:38 +0800

    stemcell: update schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.6',
+    version='2.0.7',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
