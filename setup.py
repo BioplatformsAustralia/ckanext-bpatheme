69d419ca8437c981939f1750c40cef8785428d9b
mattRedBox <matt@redboxresearchdata.com.au>
2020-04-17 16:26:46 +1000

    stemcell: updated schema

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.7',
+    version='2.0.8',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
