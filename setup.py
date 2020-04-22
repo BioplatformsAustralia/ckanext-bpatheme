a8f6f62248749ef03eddaafe29ad24161da894c6
mattRedBox <matt@redboxresearchdata.com.au>
2020-04-22 15:20:39 +1000

    bumped version for stemcell schema updates for e-tags.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.8',
+    version='2.0.9',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
