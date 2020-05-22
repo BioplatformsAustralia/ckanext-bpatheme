12274b5aafd1f39367e83a70046f55984d4563ee
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-05-22 09:35:22 +0800

    release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.10',
+    version='2.0.12',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
