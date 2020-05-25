31d4ca933824be5e2b94ddc795eecbc53f3d0411
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-05-25 17:01:27 +0800

    bulk addition of new field for ckanext-restricted

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.14',
+    version='2.0.15',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
