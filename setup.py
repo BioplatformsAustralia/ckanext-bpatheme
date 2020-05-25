93932932d13c6fee5e05d5a86aedcf6d96895f61
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-05-25 11:32:25 +0800

    commit missing file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.13',
+    version='2.0.14',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
