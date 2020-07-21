00185ad8e089fa9ce8e40cb56a07741b3841ce4f
mattRedBox <matt@redboxresearchdata.com.au>
2020-07-21 10:46:55 +1000

    release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.19',
+    version='2.0.20',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
