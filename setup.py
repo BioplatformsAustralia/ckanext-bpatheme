967e4e5bb41690d9d5c3ec74c46b5212a29591cd
mattRedBox <matt@redboxresearchdata.com.au>
2020-08-11 14:03:00 +1000

    Bumped version for release of omg pacbio genome assemblies secondary type.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.20',
+    version='2.0.21',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
