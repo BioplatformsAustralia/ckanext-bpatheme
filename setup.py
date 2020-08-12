f332099ec428727955e34b9b2a8c6aaf046cff6e
mattRedBox <matt@redboxresearchdata.com.au>
2020-08-13 08:03:04 +1000

    Rolled back code to tag 2.0.18 and added back GAP schema, for use with fix to docker-bpa-ckan tag 1.21.14, currently in prod.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.21',
+    version='2.0.18.patch_trello-462',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
