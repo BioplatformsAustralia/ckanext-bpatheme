0f931d04b0aacebecb13a7245cfaec0ca336059d
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-17 08:59:40 +1000

    bumped tag for staging beta release of new ui integration.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.34',
+    version='3.0.0',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
