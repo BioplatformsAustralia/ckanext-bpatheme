0a7a1f499632794a7cd3399884d5903299eb7922
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-17 10:52:02 +1000

    Bumped tag for release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='3.0.0',
+    version='3.0.1',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
