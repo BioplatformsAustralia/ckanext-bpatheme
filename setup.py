facfac14c2fa554f7c0ed6926e4d421f46d45d8c
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-17 11:39:20 +1000

    bumped ckanext for staging release of beta.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='3.0.1',
+    version='3.0.2',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
