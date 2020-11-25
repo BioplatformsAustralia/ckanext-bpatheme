18c7370c02b65cf19083b34d2b4bf77a13430fa2
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-25 16:40:43 +1000

    Removed redundant code from datatable poc.

--- ckanext/bpatheme/controller.py
+++ ckanext/bpatheme/controller.py
@@ -4,6 +4,9 @@ import ckan.lib.base as base
 import ckan.logic as logic
 import ckan.model as model
 from ckan.common import _, c
+import requests
+from logging import getLogger
+log = getLogger(__name__)
 
 
 CACHE_PARAMETERS = ['__cache', '__no_cache__']
@@ -12,4 +15,6 @@ CACHE_PARAMETERS = ['__cache', '__no_cache__']
 class SummaryController(base.BaseController):
 
     def index(self):
+        r = requests.get("https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1")
+        log.info("got request...")
         return base.render('summary/index.html')
