3d8f7fed2440fa26962d0381dd5a57720a98f4df
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-25 16:59:01 +1000

    poc sheets API.

--- ckanext/bpatheme/controller.py
+++ ckanext/bpatheme/controller.py
@@ -1,21 +1,33 @@
 # encoding: utf-8
+import json
+from os import path
 
 import ckan.lib.base as base
-import ckan.logic as logic
-import ckan.model as model
-from ckan.common import _, c
-import requests
+import ckan.lib.helpers as h
+# import requests
+import pandas as pd
+import numpy as np
 from logging import getLogger
+
 log = getLogger(__name__)
 
-
 CACHE_PARAMETERS = ['__cache', '__no_cache__']
+
+
+def replace_df_header_with_row(df, row):
+    df.drop(0, inplace=True)
+    df.columns = row
 
 
 class SummaryController(base.BaseController):
 
     def index(self):
-        r = requests.get("https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1")
-        log.info("got request...")
-        raise Exception("got nothing here except {0}".format(vars(r)))
-        return base.render('summary/index.html')
+        spreadsheet_data_path = '/tmp/googlesheet.json'
+        df = pd.read_json(spreadsheet_data_path)
+        first_row = df.iloc[0]
+        replace_df_header_with_row(df, first_row)
+        bt_data = df.to_json(orient="records")
+        bt_header = json.loads(first_row.to_json(orient="records"))
+
+        return base.render('summary/index.html',
+                           extra_vars={'spreadsheet_data': bt_data, 'spreadsheet_columns': bt_header})
