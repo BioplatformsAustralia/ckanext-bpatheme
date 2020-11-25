c6dc9d9e6083798662c674f8c23a2ec3da284624
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-25 16:33:57 +1000

    Removed redundant code from datatable poc.

--- ckanext/bpatheme/controller.py
+++ ckanext/bpatheme/controller.py
@@ -10,14 +10,6 @@ CACHE_PARAMETERS = ['__cache', '__no_cache__']
 
 
 class SummaryController(base.BaseController):
-    def __before__(self, action, **env):
-        try:
-            base.BaseController.__before__(self, action, **env)
-            context = {'model': model, 'user': c.user,
-                       'auth_user_obj': c.userobj}
-            logic.check_access('site_read', context)
-        except logic.NotAuthorized:
-            base.abort(403, _('Not authorized to see this page'))
 
     def index(self):
         return base.render('summary/index.html')
