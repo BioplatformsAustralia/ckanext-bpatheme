a6b01402225a3294c0e534ec306c2ff16db1205e
mattRedBox <matt@redboxresearchdata.com.au>
2020-11-25 14:27:10 +1000

    Implement routes for new summary controller.

--- ckanext/bpatheme/controller.py
+++ ckanext/bpatheme/controller.py
@@ -1,22 +1,19 @@
 # encoding: utf-8
 
-from pylons import cache
-import sqlalchemy.exc
-
-import ckan.logic as logic
-import ckan.lib.search as search
 import ckan.lib.base as base
+import ckan.logic as logic
 import ckan.model as model
-import ckan.lib.helpers as h
+from ckan.common import _, c
+from logging import getLogger
+log = getLogger(__name__)
 
-from ckan.common import _, config, c
 
 CACHE_PARAMETERS = ['__cache', '__no_cache__']
 
 
 class SummaryController(base.BaseController):
-    controller = "ckanext.bpatheme.controller:SummaryController"
     def __before__(self, action, **env):
+        log.info("Inside summary controller...")
         try:
             base.BaseController.__before__(self, action, **env)
             context = {'model': model, 'user': c.user,
@@ -26,7 +23,9 @@ class SummaryController(base.BaseController):
             base.abort(403, _('Not authorized to see this page'))
 
     def index(self):
+        log.info("Inside summary controller index...")
         return self.about()
 
     def about(self):
+        log.info("Inside summary controller about...")
         return base.render('home/about.html')
