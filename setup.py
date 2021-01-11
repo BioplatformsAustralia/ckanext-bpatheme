9c6d2cc103a9f062401d8d9818ad39ae3c0fc7e0
mattRedBox <matt@redboxresearchdata.com.au>
2021-01-12 09:45:53 +1000

    include bootstrap table libraries in package so that they appear in ckan deployment.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires=[
 
 setup(
     name='ckanext-bpatheme',
-    version='3.0.3',
+    version='3.0.4',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     package_dir={'ckanext.bpatheme': 'ckanext/bpatheme'},
     package_data={'ckanext.bpatheme': ['*.json', 'fanstatic/*.js', 'fanstatic/styles/*.css', 'fanstatic/styles/*.scss', 'templates/*.html', 'templates/*/*.html',
-                                               'templates/*/*/*.html', 'static/datacats/*.png', 'static/fonts/arial/*.eot', 'static/fonts/arial/*.svg', 'static/fonts/arial/*.woff', 'static/fonts/arial/*.woff2', 'static/fonts/arial/*.ttf', 'static/*.png', 'static/*.ico']},
+                                               'templates/*/*/*.html', 'static/datacats/*.png', 'static/fonts/arial/*.eot', 'static/fonts/arial/*.svg', 'static/fonts/arial/*.woff', 'static/fonts/arial/*.woff2', 'static/fonts/arial/*.ttf', 'static/*.png', 'static/*.ico', 'static/bootstraptable/*.js', 'static/bootstraptable/*.css']},
 
     entry_points="""
         [ckan.plugins]
