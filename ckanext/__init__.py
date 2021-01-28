76539a09029d09d8ba8a639d867b2c2c33b942d2
mattRedBox <matt@redboxresearchdata.com.au>
2021-01-29 09:58:34 +1000

    attempt at debug.

--- ckanext/__init__.py
+++ ckanext/__init__.py
@@ -2,6 +2,9 @@
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
+    import pydevd_pycharm
+
+    pydevd_pycharm.settrace('localhost', port=57891, stdoutToServer=True, stderrToServer=True)
 except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 install_requires=[
     'pandas==0.24.2',
+    'pydevd-pycharm~=193.6911.25'
 ]
 
 setup(
