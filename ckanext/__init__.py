3d6bc56befa60af91c7da9c993cd40a14535c248
mattRedBox <matt@redboxresearchdata.com.au>
2021-02-01 09:12:40 +1000

    attempt at debug.

--- ckanext/__init__.py
+++ ckanext/__init__.py
@@ -2,10 +2,8 @@
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
-    import pydevd_pycharm
     import os
 
-    pydevd_pycharm.settrace('host.docker.internal', port=int(os.environ.get("DOCKER_DEBUG_PORT")), stdoutToServer=True, stderrToServer=True, suspend=False)
 except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
