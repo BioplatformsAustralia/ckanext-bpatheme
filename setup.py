b289d9dd3aed28f64f525f88f3798c548c8ae459
Mark Tearle <mark.tearle@qcif.edu.au>
2021-04-20 09:56:11 +0800

    Release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-bpatheme',
-    version='2.0.47',
+    version='2.0.48',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
