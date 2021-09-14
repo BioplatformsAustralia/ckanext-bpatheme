43c95162241445f2bbaecc8cdc82bb2d25a5e479
mattRedBox <matt@redboxresearchdata.com.au>
2021-09-14 18:59:17 +1000

    bumped tag for PROD release of s3 window.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires=[
 
 setup(
     name='ckanext-bpatheme',
-    version='3.1.31',
+    version='3.1.32',
     description='',
     license='AGPL3',
     author='Bioplatforms Australia',
