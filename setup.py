5147f383c33d2de675bf3fa854df56614cc03491
Mark Tearle <mark.tearle@qcif.edu.au>
2021-11-12 13:42:13 +0800

    Bumped for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas==0.24.2"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.1.39",
+    version="3.1.40",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
