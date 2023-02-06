93f449370d9c4703d4894febf0610a5df1471ac8
Brigette Gonch <b.gonch@qcif.edu.au>
2023-02-07 09:53:29 +1000

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas>=1.4.3"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.4.17",
+    version="3.4.21",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
