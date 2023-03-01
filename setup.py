942fc9e69245686d10026afa847aa2cbbdbd80f2
Brigette Gonch <b.gonch@qcif.edu.au>
2023-03-01 14:15:22 +1000

    Include email templates in setup.py, bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas>=1.4.3"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.4.23",
+    version="3.4.24",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
@@ -33,6 +33,7 @@ setup(
             "templates/*/*/*.html",
             "templates/*/*/*/*.html",
             "templates/*.txt",
+            "templates/emails/*.txt",
             "templates/home/*.txt",
             "templates/external/*.css",
             "static/datacats/*.png",
