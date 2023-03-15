b4d29c0bea9429ae027c6d6a9c3b0e5344f80dd7
Brigette Gonch <b.gonch@qcif.edu.au>
2023-03-15 10:04:11 +1000

    properly commment out unneeded span to remove grey box

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas>=1.4.3"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.4.24",
+    version="3.4.25",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
