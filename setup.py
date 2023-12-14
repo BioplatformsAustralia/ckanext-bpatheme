3af1bd5349ae62bd5b33449a5ba767e025c25322
Mark Tearle <mark.tearle@qcif.edu.au>
2023-12-14 11:13:44 +0800

    Display Activity Stream for admins
    See: https://trello.com/c/X3qr9ZwY/1482-activity-stream-related-tracebacks

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas>=1.4.3"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.4.43",
+    version="3.4.44",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
