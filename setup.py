355063e5fce0e44e10e070ae315cb5ddc6d1f679
Brigette Gonch <b.gonch@qcif.edu.au>
2024-01-05 11:12:16 +1000

    Fix jinja code around display of data policy on attribution page. See Trello: https://trello.com/c/44aO816w/1817-fix-bug-with-multiple-data-policy-words-appearing-on-package-dataset-page

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["pandas>=1.4.3"]
 
 setup(
     name="ckanext-bpatheme",
-    version="3.4.47",
+    version="3.4.48",
     description="CKAN Theme for the Bioplatforms Australia Data Portal",
     license="AGPL3",
     author="Bioplatforms Australia",
